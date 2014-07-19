486f6455aed09285b81360e1994443f6922598e6
wq1 <wq1@users.noreply.github.com>
2014-07-19 21:24:15 +0900

    最終更新日と公開日が一致する場合最終更新日を非表示にするスクリプトを導入

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ $(function() {
   $.get("/footer.html", function(data) {
     $("body").append(data);
   });
+  var pubdate = $("#pubdate > *:nth-child(2)").html();
+  var moddate = $("#moddate > *:nth-child(2)").html();
+  if( pubdate !== void 0 && moddate !== void 0 ){
+    if( pubdate == moddate ){
+      $("#moddate").css("display","none")
+    }
+  }
 });
