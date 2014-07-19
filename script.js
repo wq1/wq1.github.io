090c84222d63613cce256b6c1ad1e5ca195caf64
wq1 <wq1@users.noreply.github.com>
2014-07-20 00:27:09 +0900

    スクリプトを修正

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ $(function() {
   $.get("/footer.html", function(data) {
     $("body").append(data);
   });
+});
+$(function() {
   var pubdate = $("#pubdate > *:nth-child(2)").html();
   var moddate = $("#moddate > *:nth-child(2)").html();
   if( pubdate !== void 0 && moddate !== void 0 ){
