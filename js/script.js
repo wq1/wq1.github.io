df2e2e78c40f1e2ac9cb49f4a10332de82d029a7
wq1 <wq1@users.noreply.github.com>
2014-10-05 16:00:17 +0900

    共通スクリプトを修正

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-$("title").append(" - :wq! （仮称）");
-$(function() {
+(function() {
+  $("title").append(" - :wq! （仮称）");
   $.get("/header.html", function(data) {
     $("body").prepend(data);
   });
   $.get("/footer.html", function(data) {
     $("body").append(data);
   });
-});
-$(function() {
+}());
+
+(function() {
   var pubdate = $("#pubdate > *:nth-child(2)").html();
   var moddate = $("#moddate > *:nth-child(2)").html();
   if( pubdate !== void 0 && moddate !== void 0 ){
@@ -15,4 +16,4 @@ $(function() {
       $("#moddate").css("display","none");
     }
   }
-});
+}());
