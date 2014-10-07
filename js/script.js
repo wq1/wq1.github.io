3dc649914418ffc13a7091830879db712e5331e5
wq1 <wq1@users.noreply.github.com>
2014-10-07 21:50:52 +0900

    スクリプト全般をJSLintを参考に修正

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
-(function() {
+(function () {
+  "use strict";
   $("title").append(" - :wq! （仮称）");
-  $.get("./header.html", function(data) {
+  $.get("./header.html", function (data) {
     $("main").before(data);
   });
-  $.get("./footer.html", function(data) {
+  $.get("./footer.html", function (data) {
     $("main").after(data);
   });
 }());
 
-(function() {
-  var pubdate = $("#pubdate > *:nth-child(2)").html();
-  var moddate = $("#moddate > *:nth-child(2)").html();
-  if( pubdate !== void 0 && moddate !== void 0 ){
-    if( pubdate == moddate ){
+(function () {
+  "use strict";
+  var pubdate, moddate;
+  pubdate = $("#pubdate > *:nth-child(2)").html();
+  moddate = $("#moddate > *:nth-child(2)").html();
+  if (pubdate !== undefined && moddate !== undefined) {
+    if (pubdate === moddate) {
       $("#moddate").css("display", "none");
     }
   }
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-(function() {
+(function () {
+  "use strict";
   $("#update-table").load("./update/ #update-table > *");
 }());
 
-(function() {
-  $.ajax({dataType: "text", url: "./js/script.js"});
-  $.ajax({dataType: "text", url: "./header.html"});
-  $.ajax({dataType: "text", url: "./footer.html"});
+(function () {
+  "use strict";
+  $.ajax({ dataType: "text", url: "./js/script.js" });
+  $.ajax({ dataType: "text", url: "./header.html" });
+  $.ajax({ dataType: "text", url: "./footer.html" });
 }());
