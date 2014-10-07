5c937d9f0ba2ed6f4131d009722838101abecc19
wq1 <wq1@users.noreply.github.com>
2014-10-07 21:19:31 +0900

    blog-update.jsをJSLintを参考に修正

--- js/blog-update.js
+++ js/blog-update.js
@@ -1,16 +1,16 @@
-(function() {
+(function () {
   "use strict";
   var x, y, base;
   base = "tr.m, tr.d";
   function cmd(i) { $(base).eq(i).css("display", "none"); }
   x = 0;
-  $("tr.m").each(function() {
+  $("tr.m").each(function () {
     var i, j;
     i = $(this).index(base);
     j = i - x;
-    if(j === 1) { cmd(x); }
+    if (j === 1) { cmd(x); }
     x = i;
   });
   y = $(base).length - 1;
-  if(x === y) { cmd(x); }
+  if (x === y) { cmd(x); }
 }());
