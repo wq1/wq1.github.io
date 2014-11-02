c9b58999947c0e3a3ebd499687f6ccbda2a73374
wq1 <wq1@users.noreply.github.com>
2014-11-02 21:29:47 +0900

    更新履歴と雑記の読込周りを改良

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 (function () {
   "use strict";
-  $("#update-table").load("//wq1.github.io/update/ #update-table > *");
-}());
-
-(function () {
-  "use strict";
-  function prefetch(url) {
-    $.ajax({
-      type: "GET",
-      url: url,
-      dataType: "text"
-    });
-  }
-  prefetch("//wq1.github.io/js/script.js");
-  prefetch("//wq1.github.io/header.html");
-  prefetch("//wq1.github.io/footer.html");
+  $.get("//wq1.github.io/update/", function (data) {
+    var num = 10;
+    $("#update-table > tbody").html($("#update-table tr.d", data).slice(0, num));
+    $("#update-num").html(num);
+  });
+  $.get("//wq1.github.io/blog/", function (data) {
+    var num = 5;
+    $("#blog-table > tbody").html($("#blog-table tr.d", data).slice(0, num));
+    $("#blog-num").html(num);
+  });
 }());
