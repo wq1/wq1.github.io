328ca5eaf4e9f0df142d24632ad3b5e5342111b1
wq1 <wq1@users.noreply.github.com>
2014-11-05 08:48:31 +0900

    更新履歴・雑記のrevswのスクリプトを即時関数で包括

--- js/blog-update.js
+++ js/blog-update.js
@@ -27,44 +27,47 @@
 }());
 
 
-$('#revsw').on('click', function () {
+(function () {
   'use strict';
-  var base, y, m, d, buf, y0, m0, sw;
-  base = $('tr.y, tr.m, tr.d');
 
-  y = [];
-  m = [];
-  d = [];
-  $('tr.y').each(function () { y.push(base.index(this)); });
-  $('tr.m').each(function () { m.push(base.index(this)); });
-  $('tr.d').each(function () { d.push(base.index(this)); });
-  y.reverse();
-  m.reverse();
-  d.reverse();
+  $('#revsw').on('click', function () {
+    var base, y, m, d, buf, y0, m0, sw;
+    base = $('tr.y, tr.m, tr.d');
 
-  buf = [];
+    y = [];
+    m = [];
+    d = [];
+    $('tr.y').each(function () { y.push(base.index(this)); });
+    $('tr.m').each(function () { m.push(base.index(this)); });
+    $('tr.d').each(function () { d.push(base.index(this)); });
+    y.reverse();
+    m.reverse();
+    d.reverse();
 
-  while (y.length) {
-    buf.push(base.eq(y[0])[0]);
-    y0 = y.shift();
-    while (y0 < m[0]) {
-      buf.push(base.eq(m[0])[0]);
-      m0 = m.shift();
-      while (m0 < d[0]) {
-        buf.push(base.eq(d[0])[0]);
-        d.shift();
+    buf = [];
+
+    while (y.length) {
+      buf.push(base.eq(y[0])[0]);
+      y0 = y.shift();
+      while (y0 < m[0]) {
+        buf.push(base.eq(m[0])[0]);
+        m0 = m.shift();
+        while (m0 < d[0]) {
+          buf.push(base.eq(d[0])[0]);
+          d.shift();
+        }
       }
     }
-  }
 
-  buf = $(buf);
-  $('#update-table > tbody').html(buf);
-  $('#blog-table > tbody').html(buf);
+    buf = $(buf);
+    $('#update-table > tbody').html(buf);
+    $('#blog-table > tbody').html(buf);
 
-  sw = $(this);
-  if (sw.html() === '▲') {
-    sw.html('▼');
-  } else {
-    sw.html('▲');
-  }
-});
+    sw = $(this);
+    if (sw.html() === '▲') {
+      sw.html('▼');
+    } else {
+      sw.html('▲');
+    }
+  });
+}());
